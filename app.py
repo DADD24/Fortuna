52a397fcbb91a518515533eda5c8ae885355ba3b
HDTHREE <hd3nnis4@gmail.com>
2025-08-08 20:53:39 -0400

    debug = false

--- app.py
+++ app.py
@@ -1831,4 +1831,4 @@ def load_history(is_open, user_id):
 
 # --- Main Execution ---
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8050)
+    app.run(debug=False, host="0.0.0.0", port=8050)
